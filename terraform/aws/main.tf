fafebae21136257e6a02b3074f8a6622c186039f
mmyszak999 <michalmyszak@onet.eu>
2026-05-13 12:53:02 +0200

    refactor tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,7 +1,8 @@
+
 resource "random_id" "suffix" {
   byte_length = 4
 }
-
+/*
 # S3 bucket
 resource "aws_s3_bucket" "ml_bucket" {
   bucket = "mlops-thesis-${random_id.suffix.hex}"
@@ -16,3 +17,5 @@ resource "aws_cloudwatch_log_group" "logs" {
   name              = "/aws/sagemaker/mlops-thesis-${random_id.suffix.hex}"
   retention_in_days = 7
 }
+
+*/
--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,7 +1,7 @@
-output "bucket_name" {
-  description = "S3 bucket name"
-  value       = aws_s3_bucket.ml_bucket.bucket
-}
+#output "bucket_name" {
+#  description = "S3 bucket name"
+#  value       = aws_s3_bucket.ml_bucket.bucket
+#}
 
 output "sagemaker_role_arn" {
   description = "IAM role for SageMaker"
