b1e951f45f321a3c3bfc9ac3f86bf34487202b87
mmyszak999 <michalmyszak@onet.eu>
2026-05-13 13:21:32 +0200

    add python files

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,9 +1,7 @@
-
 resource "random_id" "suffix" {
   byte_length = 4
 }
-/*
-# S3 bucket
+
 resource "aws_s3_bucket" "ml_bucket" {
   bucket = "mlops-thesis-${random_id.suffix.hex}"
 
@@ -12,10 +10,15 @@ resource "aws_s3_bucket" "ml_bucket" {
   }
 }
 
-# CloudWatch logs
-resource "aws_cloudwatch_log_group" "logs" {
-  name              = "/aws/sagemaker/mlops-thesis-${random_id.suffix.hex}"
-  retention_in_days = 7
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.ml_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-*/
+resource "aws_cloudwatch_log_group" "sagemaker_logs" {
+  name              = "/aws/sagemaker/mlops-thesis"
+  retention_in_days = 7
+}
