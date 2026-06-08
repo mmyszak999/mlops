12b93df7f189547f815ee9840575167be37f3739
mmyszak999 <michalmyszak@onet.eu>
2026-06-08 20:54:57 +0200

    refactor ml dir

--- ml/predict.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-def model_fn(model_dir):
-    model = joblib.load(f"{model_dir}/model.pkl")
-    return model
-
-def predict_fn(input_data, model):
-    prediction = model.predict(input_data)
-    return prediction
