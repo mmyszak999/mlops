570246673d9a14d4b9d2d7569cd47b127177eab0
mmyszak999 <michalmyszak@onet.eu>
2026-08-06 14:45:37 +0200

    refactor deploy.py

--- ml/aws/deploy.py
+++ ml/aws/deploy.py
@@ -2,14 +2,28 @@ import sagemaker
 
 from sagemaker.sklearn.model import SKLearnModel
 
+# =========================
+# Model version
+# =========================
+
+MODEL_VERSION = "v1"
+
+# =========================
+# AWS configuration
+# =========================
+
 role = "arn:aws:iam::154959838069:role/sagemaker-execution-role-d37b3fa3"
 
 bucket = "mlops-thesis-d37b3fa3"
 
 session = sagemaker.Session()
 
+# =========================
+# Deploy model
+# =========================
+
 model = SKLearnModel(
-    model_data=f"s3://{bucket}/models/model.tar.gz",
+    model_data=f"s3://{bucket}/models/{MODEL_VERSION}/model.tar.gz",
     role=role,
     framework_version="1.2-1",
     entry_point="ml/aws/predict.py",
@@ -22,4 +36,4 @@ predictor = model.deploy(
     instance_type="ml.t2.medium"
 )
 
-print("Endpoint deployed")
+print(f"Model {MODEL_VERSION} deployed successfully")
