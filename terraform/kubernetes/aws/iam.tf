d6ef0adf3f57a746eb56b7eef8c658376ed7db58
mmyszak999 <michalmyszak@onet.eu>
2026-08-18 17:42:03 +0200

    refactor k8s files

--- terraform/kubernetes/aws/iam.tf
+++ terraform/kubernetes/aws/iam.tf
@@ -160,3 +160,71 @@ resource "aws_eks_pod_identity_association" "inference" {
     aws_eks_addon.pod_identity_agent
   ]
 }
+
+# =========================================================
+# MLFLOW POD ROLE
+# =========================================================
+
+resource "aws_iam_role" "mlflow_pod" {
+  name = "${var.project_name}-mlflow-pod-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "mlflow_s3" {
+  name = "${var.project_name}-mlflow-s3"
+  
+
+  role = aws_iam_role.mlflow_pod.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:AbortMultipartUpload"
+        ]
+
+        Resource = [
+          aws_s3_bucket.models.arn,
+          "${aws_s3_bucket.models.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_eks_pod_identity_association" "mlflow" {
+  cluster_name    = aws_eks_cluster.k8s.name
+  namespace       = "mlops"
+  service_account = "mlflow"
+  role_arn        = aws_iam_role.mlflow_pod.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent
+  ]
+}
