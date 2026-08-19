b07fe2e910fcddf3bbab456de9e1d0e84c857ae9
mmyszak999 <michalmyszak@onet.eu>
2026-08-19 15:10:16 +0200

    refactor k8s infra pipeline

--- ml/aws/test_endpoint.py
+++ ml/aws/test_endpoint.py
@@ -1,52 +1,29 @@
 import boto3
 import json
 
+
 runtime = boto3.client(
     "sagemaker-runtime",
     region_name="us-east-1"
 )
 
-payload = [[
-    1,        # Pclass
-    12,       # Age
-    0,        # SibSp
-    0,        # Parch
-    1311.2833,  # Fare
-    1,        # Sex_female
-    0,        # Sex_male
-    0,        # Embarked_C
-    1,        # Embarked_Q
-    0         # Embarked_S
-]]
-"""
-    3,
-    45,
-    0,
-    0,
-    7.25,
-    0,
-    1,
-    0,
-    0,
-    1
 
-    2,
-    30,
-    1,
-    1,
-    25,
-    1,
-    0,
-    1,
-    0,
-    0
+payload = {
+    "Pclass": 1,
+    "Sex": "female",
+    "Age": 12,
+    "SibSp": 0,
+    "Parch": 0,
+    "Fare": 1311.2833,
+    "Embarked": "Q"
+}
 
 
-"""
 response = runtime.invoke_endpoint(
     EndpointName="mlops-thesis-endpoint",
     ContentType="application/json",
     Body=json.dumps(payload)
 )
+
 
 print(response["Body"].read().decode())
