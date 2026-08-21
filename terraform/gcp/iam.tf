3a5be456ab9ab42c9ac0e0a0482470428d7d5195
mmyszak999 <michalmyszak@onet.eu>
2026-08-21 16:38:04 +0200

    refactor gcp iam

--- terraform/gcp/iam.tf
+++ terraform/gcp/iam.tf
@@ -1,15 +1,24 @@
+data "google_project" "current" {
+  project_id = var.project_id
+}
+
+resource "google_project_service_identity" "vertex_ai" {
+  project  = var.project_id
+  service  = "aiplatform.googleapis.com"
+
+  depends_on = [
+    google_project_service.required["aiplatform.googleapis.com"]
+  ]
+}
+
 resource "google_storage_bucket_iam_member" "vertex_ai_reader" {
   bucket = google_storage_bucket.models.name
 
   role = "roles/storage.objectViewer"
 
-  member = "serviceAccount:service-${data.google_project.current.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
+  member = google_project_service_identity.vertex_ai.member
 
   depends_on = [
-    google_project_service.required
+    google_project_service_identity.vertex_ai
   ]
-}
-
-data "google_project" "current" {
-  project_id = var.project_id
 }
