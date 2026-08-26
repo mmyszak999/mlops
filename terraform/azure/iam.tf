8b944817c8eb535c64869f1a2c9c56a73ac0ed76
mmyszak999 <michalmyszak@onet.eu>
2026-08-26 15:31:53 +0200

    fix tf fmt

--- terraform/azure/iam.tf
+++ terraform/azure/iam.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "workspace_storage" {
   scope                = azurerm_storage_account.ml.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id = azurerm_machine_learning_workspace.ml.identity[0].principal_id
+  principal_id         = azurerm_machine_learning_workspace.ml.identity[0].principal_id
 }
